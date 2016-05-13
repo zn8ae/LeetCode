896a5635e380737feaca25a9a43b6c58d70a0023
Zihan <zn8ae@virginia.edu>
2016-05-13 01:12:39 -0400

    Add comments

--- 67.Add-Binary/JavaSolution2.java
+++ 67.Add-Binary/JavaSolution2.java
@@ -6,17 +6,30 @@
 // Return "100".
 public class Solution {
     public String addBinary(String a, String b) {
+        // we only process the situation when b is shorter.
+        // if a is shorter, switch two strings.
         if(a.length() < b.length()){
             String tmp = a;
             a = b;
             b = tmp;
         }
         
+        //initialize variable
+        //pointer to the tails of a and b so that we can add
+        //number in same digit level
+        //carries to save carries if 1+1 = 10
+        //rst to store result
         int pa = a.length()-1;
         int pb = b.length()-1;
         int carries = 0;
         String rst = "";
         
+        //since b is shorter, we process b first otherwise we'll get out of range
+        //parse two tail digit to 0 and add them together with carries from last time
+        //add the result to result. Notice here we are adding to the left of result. 
+        //Since we are going larger and larger with resect to digit level
+        //process carries after we add them on
+        //decrease pointers
         while(pb >= 0){
             int sum = (int)(a.charAt(pa) - '0') + (int)(b.charAt(pb) - '0') + carries;
             rst = String.valueOf(sum % 2) + rst;
@@ -25,6 +38,7 @@ public class Solution {
             pb --;
         }
         
+        //do the same for pa only since b is done
         while(pa >= 0){
             int sum = (int)(a.charAt(pa) - '0') + carries;
             rst = String.valueOf(sum % 2) + rst;
@@ -32,8 +46,11 @@ public class Solution {
             pa --;
         }       
         
+        //if we have a carry digit left, we add them to final result
         if (carries == 1)
             rst = "1" + rst;
+            
+        //Congratulations. Good to go!!!
         return rst;
     }
 }
