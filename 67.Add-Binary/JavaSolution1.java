ef03fb9e208d4aa7954553271426f1d3d53efd63
Zihan <zn8ae@virginia.edu>
2016-05-13 01:13:47 -0400

    Commented

--- 67.Add-Binary/JavaSolution1.java
+++ 67.Add-Binary/JavaSolution1.java
@@ -6,11 +6,11 @@ public class Solution {
         //Constructor can take in String input type and specify binary
         //BigInteger can store over 32bits value
         
-        BigInteger aValue = new BigInteger(a, 2);
-	BigInteger bValue = new BigInteger(b, 2);
-	BigInteger sum = new BigInteger("0", 2);
-	sum = aValue.add(bValue);
-	return sum.toString(2);
+        BigInteger aValue = new BigInteger(a, 2); // turn a to binary biginteger
+	BigInteger bValue = new BigInteger(b, 2);// turn b to binary biginger
+	BigInteger sum = new BigInteger("0", 2);// initialize sum as binary big ineger
+	sum = aValue.add(bValue);//sum = a + b
+	return sum.toString(2);//change sum to string. Keep binary. Done!
 		
 		
     }
